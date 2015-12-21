443361aa03845fffa0ff862b9ee74e157c4bdf29
Jean-François Auger <nechry@gmail.com>
2015-12-21 23:38:40 +0100

    Update AlarmMonitoringSensor.ino
    change setup order for pins definition

--- AlarmMonitoringSensor.ino
+++ AlarmMonitoringSensor.ino
@@ -4,35 +4,35 @@
   Contact: nechry@gmail.com
   License: GNU LGPL 2.1+
   https://github.com/nechry/AlarmMonitoringSensor/
- 
+
   Purpose:
- 
-  This is a monitoring system for home alarm control panel, base on mySensors 
-  I use Light Sensors to monitor alarm leds: 
-    operation mode, 
-	trigger, 
-	bell is activated, 
+
+  This is a monitoring system for home alarm control panel, base on mySensors
+  I use Light Sensors to monitor alarm leds:
+    operation mode,
+	trigger,
+	bell is activated,
 	maintenance.
   The LEDs have tree states, Off, blink and On
- 
+
   INPUT:
   Pin 3, 4, 5 and 6 for led sensor.
   OUTPUT:
   Pin 8 and 7 to report the current operation mode.
-  
+
   NRF24L01+:
   See http://www.mysensors.org/build/connect_radio for more details
-  9	    CE	
+  9	    CE
   10	CSN/CS
   13	SCK
   11	MOSI
   12	MISO
   2	    IRQ
-  
+
 */
 
 #include <SPI.h>
-#include <MySensor.h>  
+#include <MySensor.h>
 
 #define Operation_Child_Id 0
 #define Trigger_Child_Id 1
@@ -71,11 +71,12 @@ int lastLedMaintenance = Unknown;
 
 // define, how many sensors
 #define SENSORS 4
-// add each led sensor 
+// add each led sensor
 int pins[SENSORS] = { Operation_Sensor_Pin, Trigger_Sensor_Pin, Bell_Sensor_Pin, Maintenance_Sensor_Pin };
 
 // the current state of each sensor (off, blink, on)
 int states[SENSORS];
+int pending_states[SENSORS];
 
 // how many blinks have been counted on each sensor
 int blinks[SENSORS];
@@ -86,26 +87,17 @@ int blinks[SENSORS];
 // Reporting interval on serial port, in milliseconds
 #define REPORT_INTERVAL 5000
 
-void setup()  
+void setup()
 {
   Serial.begin(SERIAL_SPEED);
-  /* initialize each led sensor */
-  for (int i = 0; i < SENSORS; i++) {
-    pinMode(pins[i], INPUT);
-    states[i] = HIGH;
-    blinks[i] = 0;
-  }
-  //setup led status as output for mode
-  pinMode(Red_Led_Pin, OUTPUT);   
-  pinMode(Blue_Led_Pin, OUTPUT); 
-  //desactivate leds
-  digitalWrite(Red_Led_Pin,HIGH);  
-  digitalWrite(Blue_Led_Pin,HIGH);
+
+
+
   //start comm with gateway
-  gw.begin();
+  gw.begin(NULL, 1);
 
   // Send the sketch version information to the gateway and Controller
-  gw.sendSketchInfo("Alarm Monitoring Sensor", "1.2");
+  gw.sendSketchInfo("Alarm Monitoring Sensor", "1.1");
 
   // Register all sensors to gateway (they will be created as child devices)
   gw.present(Operation_Child_Id, S_LIGHT_LEVEL);
@@ -113,23 +105,38 @@ void setup()
   gw.present(Bell_Child_Id, S_LIGHT_LEVEL);
   gw.present(Maintenance_Child_Id, S_LIGHT_LEVEL);
 
+
+  /* initialize each led sensor */
+  for (int i = 0; i < SENSORS; i++) {
+    pinMode(pins[i], INPUT);
+    states[i] = HIGH;
+    pending_states[i] = HIGH;
+    blinks[i] = 0;
+  }
+  //setup led status as output for mode
+  pinMode(Red_Led_Pin, OUTPUT);
+  pinMode(Blue_Led_Pin, OUTPUT);
+  //desactivate leds
+  digitalWrite(Red_Led_Pin, HIGH);
+  digitalWrite(Blue_Led_Pin, HIGH);
+
   //do a small blink effect as startup completed
-  digitalWrite(Red_Led_Pin,LOW);
+  digitalWrite(Red_Led_Pin, LOW);
   delay(250);
-  digitalWrite(Red_Led_Pin,HIGH);
-  delay(250);  
-  digitalWrite(Blue_Led_Pin,LOW);  
+  digitalWrite(Red_Led_Pin, HIGH);
   delay(250);
-  digitalWrite(Blue_Led_Pin,HIGH);
+  digitalWrite(Blue_Led_Pin, LOW);
+  delay(250);
+  digitalWrite(Blue_Led_Pin, HIGH);
 }
 
 unsigned long loops = 0;
 unsigned long next_print = millis() + REPORT_INTERVAL;
-
-void loop()      
-{ 
+bool lastLedOperation_skip = true;
+void loop()
+{
   // go through each LED sensor
-  for (int index = 0; index < SENSORS; index++) {    
+  for (int index = 0; index < SENSORS; index++) {
     // read current value
     int ledState = digitalRead(pins[index]);
     // check if state is changed:
@@ -161,86 +168,90 @@ void loop()
         Serial.print(" ");
         //led interpreter
         int currentLedStatus = Unknown;
-        if(blinks[index] > 1){
+        if (blinks[index] > 1) {
           currentLedStatus = Blinking;
           Serial.println("Partial");
         }
-        else if(states[index] == LOW){
+        else if (states[index] == LOW) {
           currentLedStatus = Activated;
           Serial.println("Activated");
         }
-        else{
-          currentLedStatus = Desactivated;     
-          Serial.println("Desactivated");     
+        else {
+          currentLedStatus = Desactivated;
+          Serial.println("Desactivated");
         }
 
         //report to gateway for each sensor if changes
-        switch(index){
-        case 0 :
-          {
-            //check last alarm mode
-            if (currentLedStatus != lastLedOperation) {
-              //reset led mode
-              digitalWrite(Red_Led_Pin,HIGH);
-              digitalWrite(Blue_Led_Pin,HIGH);
-              //activate leds
-              switch(currentLedStatus){
-              case Blinking:
-                digitalWrite(Blue_Led_Pin,LOW);
-                break;
-              case Activated:
-                digitalWrite(Red_Led_Pin,LOW);
-                break;
-              default:
-                Serial.println("Unknown");
-                break;
+        switch (index) {
+          case 0 :
+            {
+              //check last alarm mode
+              if (currentLedStatus != lastLedOperation) {
+                if (lastLedOperation_skip) {
+                  lastLedOperation_skip = false;
+                }
+                else {
+                  lastLedOperation_skip = true;
+                  //reset led mode
+                  digitalWrite(Red_Led_Pin, HIGH);
+                  digitalWrite(Blue_Led_Pin, HIGH);
+                  //activate leds
+                  switch (currentLedStatus) {
+                    case Blinking:
+                      digitalWrite(Blue_Led_Pin, LOW);
+                      break;
+                    case Activated:
+                      digitalWrite(Red_Led_Pin, LOW);
+                      break;
+                    default:
+                      Serial.println("Unknown");
+                      break;
+                  }
+                  //send a report to the gateway
+                  gw.send(messageOperation.set(currentLedStatus));
+                  //save led operation
+                  lastLedOperation = currentLedStatus;
+                }
               }
-              //send a report to the gateway
-              gw.send(messageOperation.set(currentLedStatus));
-              //save led operation
-              lastLedOperation = currentLedStatus;
+              break;
             }
-            break;
-          }
-        case 1:
-          {
-            //check last alarm trigger
-            if(lastLedTrigger !=currentLedStatus){
-              //send report to gateway
-              gw.send(messageTrigger.set(currentLedStatus));
-              //save led trigger
-              lastLedTrigger = currentLedStatus;
+          case 1:
+            {
+              //check last alarm trigger
+              if (lastLedTrigger != currentLedStatus) {
+                //send report to gateway
+                gw.send(messageTrigger.set(currentLedStatus));
+                //save led trigger
+                lastLedTrigger = currentLedStatus;
+              }
+              break;
             }
-            break;
-          }
-        case 2:
-          {
-            //check last alarm raise
-            if(lastLedBell !=currentLedStatus){
-              //send report to gateway
-              gw.send(messageRaise.set(currentLedStatus));
-              //save led bell state
-              lastLedBell = currentLedStatus;
+          case 2:
+            {
+              //check last alarm raise
+              if (lastLedBell != currentLedStatus) {
+                //send report to gateway
+                gw.send(messageRaise.set(currentLedStatus));
+                //save led bell state
+                lastLedBell = currentLedStatus;
+              }
+              break;
             }
-            break;
-          }
-        case 3:
-          {
-            if(lastLedMaintenance != currentLedStatus){
-              //send report to gateway
-              gw.send(messageMaintenance.set(currentLedStatus));
-              //save led maintenance
-              lastLedMaintenance = currentLedStatus;
+          case 3:
+            {
+              if (lastLedMaintenance != currentLedStatus) {
+                //send report to gateway
+                gw.send(messageMaintenance.set(currentLedStatus));
+                //save led maintenance
+                lastLedMaintenance = currentLedStatus;
+              }
+              break;
             }
-            break;
-          }
         }
         //reset the blink counter
-        blinks[index] = 0;        
+        blinks[index] = 0;
       }
       Serial.println("-"); // end marker
     }
   }
 }
-
-
